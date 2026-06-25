297887da5560bf471b9c690bdf591187c54f0cdd
footlooseshaman <shopping.sudhanshu@gmail.com>
2026-06-25 17:47:34 +0000

    Add multi-param and keyword argument functions

--- functions_practice.py
+++ functions_practice.py
@@ -4,3 +4,27 @@ def check_status(device_name):
 
 result = check_status("Router1")
 print(result)
+
+
+device = {
+    "host": "192.168.1.1",
+    "username": "admin",
+    "password": "cisco123",
+    "device_type": "cisco_ios"
+}
+
+print(device["host"])
+print(device["device_type"])
+
+# Change an existing value
+device["password"] = "newpass456"
+
+# Add a brand-new key-value pair
+device["location"] = "Mumbai-DC"
+
+print(device["password"])
+print(device["location"])
+#print(device["model"])
+
+print(device.get("model"))
+print(device.get("model", "unknown"))
