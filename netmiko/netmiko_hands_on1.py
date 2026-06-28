d74c6dfd4677edcbfdc243ed904676a06b6c4c93
footlooseshaman <shopping.sudhanshu@gmail.com>
2026-06-28 16:30:56 +0000

    added a practice folder

---
+++ netmiko/netmiko_hands_on1.py
@@ -0,0 +1,30 @@
+from netmiko import ConnectHandler
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+device = {
+    "device_type" : "cisco_xe",
+    "host" : os.getenv("NETMIKO_HOST"),
+    "username" : os.getenv("NETMIKO_USERNAME"),
+    "password" : os.getenv("NETMIKO_PASSWORD"),
+}
+
+commands = [
+    "show ip interface brief",
+    "show version",
+    "show ip route",
+]
+
+connection = ConnectHandler(**device)
+
+for command in commands:
+    print(f"\n=== OUTPUT OF: {command} ====")
+    output = connection.send_command(command)
+    print(output)
+
+
+
+# disconnects from the ssh
+connection.disconnect()
